51b9699bd7fb37a5cf13818e2e98c79500530779
La <rer@ya.ru>
2019-11-08 10:25:36 +0300

    do slight recomposition

---
+++ NoteBook/Fields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteBook
+{
+    class Fields
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string FathersName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string Miscellaneous { get; set; }
+    }
+}
